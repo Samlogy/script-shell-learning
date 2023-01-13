abd2673f42804b85e5a7f1df7c8e5e4d7c36ea3d
samy senani <aceportgasd235@gmail.com>
2023-01-13 10:58:26 +0100

    add: constVars

---
+++ constVars.sh
@@ -0,0 +1,6 @@
+echo "Variables / constants"
+
+name='sam' # can be a variable / constant
+
+# print message
+echo "hello $name !"
---
+++ index.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+# variable / constants
+# arithmetic operations
+# script arguments
+# Numeric Comparison logical operators + Conditions
+# Conditional Statements (Decision Making)
+# Loops
