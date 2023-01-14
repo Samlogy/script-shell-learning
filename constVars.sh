348f38d7c8ccc272a1d71091b5cd2670a54deebe
samy senani <aceportgasd235@gmail.com>
2023-01-14 07:56:46 +0100

    small editions (permission update)

--- constVars.sh
+++ constVars.sh
@@ -1,6 +1,22 @@
 echo "Variables / constants"
 
+declare -r NUM=5
+
 name='sam' # can be a variable / constant
+number=6
 
 # print message
 echo "hello $name !"
+echo "constant $NUM"
+
+# increment / decrement
+
+let number+=5
+let number++
+let ++number
+
+let number-=5
+let number--
+let --number
+
+echo "increment $number"
