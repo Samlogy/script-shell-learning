d7492926952d7ee1ad21c21cadc55af10f5f03aa
samy senani <aceportgasd235@gmail.com>
2023-01-14 09:11:28 +0100

    add: script args example

--- scriptArgs.sh
+++ scriptArgs.sh
@@ -32,3 +32,43 @@ echo your age is: $variable_name
 # pass argument -> script shell (positional arguments)
 echo "Passing arguments --> Script Shell"
 echo "age: $1 & field: $2"
+
+
+## Example 3:
+## Read multiple values
+read -p "Enter 3 Numbers to Sum : " num1 num2 num3
+	
+sum=$((num1+num2+num3))
+
+echo "$num1 + $num2 + $num3 = $sum"
+
+# Hide the input with the s code
+read -sp "Enter the Secret Code " secret
+
+if [ "$secret" == "password" ]; then
+    echo "SUCCESS !"
+else
+    echo "Wrong Password"
+fi
+
+
+## Example 4:
+# Print the first argument
+echo "1st Argument : $1"
+
+sum=0
+
+# $# tells you the number of arguments
+while [[ $# -gt 0 ]]; do
+
+    # Get the first argument
+    num=$1
+    sum=$((sum + num))
+    
+    # shift moves the value of $2 into $1 until none are left
+    # The value of $# decrements as well
+    shift
+done
+
+echo "Sum : $sum"
+	
