025272a25a492ee8db9c0e1cff8bbea3fc22bbac
samy senani <aceportgasd235@gmail.com>
2023-01-14 09:13:04 +0100

    add: execute linux cli inside script shell

--- execCmd.sh
+++ execCmd.sh
@@ -1,2 +1,10 @@
+# Execute linux command lines inside our script
+
 var=`df -h | grep tmpfs`
 echo $var
+
+## creact new_file and open it iwth gedit
+touch newFile && gedit newFile  
+
+##  if newDir does not exist then create it
+[-d newDir ] || mkdir newDir    
