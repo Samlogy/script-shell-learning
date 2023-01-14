d0c93ef1b16423af1cd6fa59d7f3e7646b26600c
samy senani <aceportgasd235@gmail.com>
2023-01-14 07:33:48 +0100

    add: examples (diffrent syntaxes)

--- conditions.sh
+++ conditions.sh
@@ -5,17 +5,50 @@
 
 # AND -a OR -o
 
-read a
-read b
-read c
 
-if [ $a == $b -a $b == $c -a $a == $c ]
+## Example 1:
+# read -p "Enter a number : " num
+ 	
+# if ((num == 10)); then
+#     echo "Your number equals 10"
+# fi
+
+# if ((num > 10)); then
+#     echo "It is greater then 10"
+# elif ((num < 10)); then
+#     echo "It is less then 10"
+# else
+#     echo "It is equal to 10"
+# fi
+
+# if (( ((num % 2)) == 0 )); then
+#     echo " It is even"
+# fi
+
+# # You can use logical operators like &&, || and !
+# if (( ((num > 0)) && ((num < 11)) )); then
+#     echo "$num is between 1 and 10"
+# fi
+
+
+## Example 2:
+read -p "What is your name? " name
+echo "Hello $name"
+
+read -p "How old are you? " age
+if [ $age -ge 19 ]
 then
-echo EQUILATERAL
+    echo "You can drive"
 
-elif [ $a == $b -o $b == $c -o $a == $c ]
-then 
-echo ISOSCELES
+# Check another condition
+elif [ $age -eq 18 ]
+then
+    echo "You can drive next year"
+    
+# Executed by default
 else
-echo SCALENE
+    echo "You can't drive"
+    
+# Closes the if statement
 fi
+ 	
